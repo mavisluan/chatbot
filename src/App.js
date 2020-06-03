d17f835de91b71c7e6c84284b5bf343472cfa327
yi <mavisluan83@gmail.com>
2020-06-03 12:45:26 -0700

    Added scroll to bottom feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ Amplify.configure(awsconfig);
 
 const App = () =>  {
     return (
-      <div className="mt-5">
+      <div className="mt-3">
         <header className="App-header">
           <h3 className="App-title text-center">Welcome to ChatBot Demo</h3>
         </header>
--- src/StyledOrderCoffee.js
+++ src/StyledOrderCoffee.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {Alert, Container} from 'react-bootstrap'
 import { Interactions } from 'aws-amplify';
 import { Message } from 'react-chat-ui'
 
-class StyledOrderCoffee extends Component {
-  state = {
-    input: '',
+const StyledOrderCoffee = () => {
+  const [msg, setMsg] = useState('')
+  const [chat, setChat] = useState({
     finalMessage: '',
     messages: [
       new Message({
@@ -14,45 +14,54 @@ class StyledOrderCoffee extends Component {
         senderName:'robot'
       })
     ]
-  }
-  _handleKeyPress = (e) => {
+  })
+  const { finalMessage, messages} = chat;
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages.length])
+
+  const _handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.submitMessage()
+      submitMessage()
     }
   }
-  onChange(e) {
-    const input = e.target.value
-    this.setState({
-      input
-    })
+  
+  const onChange =(e)=> {
+    setMsg(e.target.value)
   }
 
-  async submitMessage() {
-    const { input } = this.state
-    if (input === '') return
+  const submitMessage = async () => {
+    if (msg === '') return
     const message = new Message({
       id: 0,
-      message: input,
+      message: msg,
       senderName: 'user'
     })
-    let messages = [...this.state.messages, message]
 
-    this.setState({
-      messages,
-      input: ''
+    console.log('user message');
+    let messages = [...chat.messages, message]
+    setMsg('')
+    setChat({
+      ...chat,
+      messages
     })
-    const response = await Interactions.send("CoffeeBot_dev", input);
+
+    const response = await Interactions.send("CoffeeBot_dev", msg);
     const responseMessage = new Message({
       id: 1,
       message: response.message,
       senderName: 'robot'
     })
-    messages  = [...this.state.messages, responseMessage]
-    this.setState({ messages })
+
+    console.log('robot message');
+
+    setChat({
+      ...chat,
+      messages: [ ...messages, responseMessage]
+    })
   }
 
-  render() {
-    console.log('messages', this.state.messages)
     const background = (senderName) => {
       return (senderName === 'robot') ? 'secondary' : 'success'
     }
@@ -64,28 +73,33 @@ class StyledOrderCoffee extends Component {
     const height = window.screen.height * 0.65
     const width = window.screen.width * 0.5
 
+    const messagesEndRef = useRef(null)
+    const scrollToBottom = () => {
+      messagesEndRef.current.scrollIntoView({bahavior: 'smooth'})
+    }
+
     return (
-      <Container className='mt-5' style={{height: height, width: width, overflow:'scroll'}}>
-          <h2 className='text-danger'>{this.state.finalMessage}</h2>
+      <Container className='mt-2' style={{height: height, width: width, overflow:'scroll'}}>
+          <h2 className='text-danger'>{finalMessage}</h2>
           <div> 
-            {this.state.messages.map((msg, index) => (
+            {messages.map((msg, index) => (
               <div key={index} className='clearfix'>
               <Alert variant={background(msg.senderName)} className={textAlign(msg.senderName)} style={{maxWidth: '80%'}}> 
                 {msg.message}
               </Alert>
               </div>
             ))}
-          </div>
+          </div> 
           <input 
             className='form-control fixed-bottom mx-auto mb-3' 
-            onKeyPress={this._handleKeyPress}
-            onChange={this.onChange.bind(this)}
-            value={this.state.input}
+            onKeyPress={_handleKeyPress}
+            onChange={onChange}
+            value={msg}
             style={{width: width}}
           />
+          <div ref={messagesEndRef} type='text'/>
       </Container>
     );
-  }
 }
 
 export default StyledOrderCoffee
